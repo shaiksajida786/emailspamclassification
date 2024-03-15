5a3d4ffe00649544e6c50d1c60d2b2850b12cde0
shaiksajida786 <137096217+shaiksajida786@users.noreply.github.com>
2024-03-15 16:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+
+nltk.download('stopwords')
+
 #nltk.download()
 #nltk.data.path.append("\AppData\Roaming\nltk_data")
 from nltk.stem.porter import PorterStemmer
